ad71bd76b058ac30928099485a26e5e8dc2be667
JohnCruz <john.cruz6725@gmail.com>
2023-01-24 18:28:41 -0600

    .

---
+++ resume/router.py
@@ -0,0 +1,13 @@
+from rest_framework import routers
+from info_api.api.viewsets import * 
+
+router = routers.DefaultRouter()
+#router.register(r'person', PersonViewSet)
+
+'''
+
+router.register(r'resume', ResumeViewSet)
+router.register(r'education', EducationViewSet)
+router.register(r'professional', ProfessionalViewSet)
+router.register(r'project', ProjectViewSet)
+'''
--- resume/urls.py
+++ resume/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("info_api/", include("info_api.urls")),
+    path("", include("info_api.urls")),
     path("admin/", admin.site.urls),
-    path("", include("frontpage.urls")),
+    path("api-auth", include("rest_framework.urls")),
 ]
